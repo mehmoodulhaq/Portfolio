3154ff9d6c920e8b6c75090832c040b22acb4c3a
imharisaa <hsmeh.dev@gmail.com>
2025-04-27 12:48:41 +0300

    some changes

--- server.js
+++ server.js
@@ -38,3 +38,43 @@ app.prepare().then(() => {
       console.log(`> Ready on http://${hostname}:${port}`)
     })
 })
+
+// const { createServer } = require('http')
+// const { parse } = require('url')
+// const next = require('next')
+
+// // Set environment variables for production
+// const dev = process.env.NODE_ENV !== 'production'
+// const hostname = dev ? 'localhost' : '0.0.0.0'  // cPanel usually uses 0.0.0.0
+// const port = process.env.PORT || 3000  // Use process.env.PORT provided by cPanel
+
+// const app = next({ dev, hostname, port })
+// const handle = app.getRequestHandler()
+
+// app.prepare().then(() => {
+//   createServer(async (req, res) => {
+//     try {
+//       const parsedUrl = parse(req.url, true)
+//       const { pathname, query } = parsedUrl
+
+//       if (pathname === '/a') {
+//         await app.render(req, res, '/a', query)
+//       } else if (pathname === '/b') {
+//         await app.render(req, res, '/b', query)
+//       } else {
+//         await handle(req, res, parsedUrl)
+//       }
+//     } catch (err) {
+//       console.error('Error occurred handling', req.url, err)
+//       res.statusCode = 500
+//       res.end('internal server error')
+//     }
+//   })
+//     .once('error', (err) => {
+//       console.error(err)
+//       process.exit(1)
+//     })
+//     .listen(port, () => {
+//       console.log(`> Ready on http://${hostname}:${port}`)
+//     })
+// })
--- src/lib/content/about.ts
+++ src/lib/content/about.ts
@@ -22,5 +22,5 @@ export const aboutSection: AboutSectionType = {
 
     ],
     },
-    img: '/mehmood.jpeg',
+    img: '/mehmood.png',
 };
